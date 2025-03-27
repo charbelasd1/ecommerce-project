e12408f0fc9cbe7e0e01a8fdb0684de34aac285f
charbelasd1 <charbel.assaad@net.usek.edu.lb>
2025-03-27 19:34:48 +0200

    Update footer styling and layout with email signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { environment } from '../env/env.dev';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListingModule } from './features/product-listing/product-listing.module';
-import { StoreModule } from '@ngrx/store';
-import { authReducer } from './core/auth/state/auth.reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { AuthEffect } from './core/auth/state/auth.effects';
-import { AuthModule } from './core/auth/auth.module';
-import { HomeModule } from './features/home-page/home.module';
 import { AppShellModule } from './core/app-shell/app-shell.module';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { AuthModule } from './core/auth/auth.module';
+import { AuthEffect } from './core/auth/state/auth.effects';
+import { authReducer } from './core/auth/state/auth.reducers';
+import { HomeModule } from './features/home-page/home.module';
+import { ProductListingModule } from './features/product-listing/product-listing.module';
 
 @NgModule({
   declarations: [
